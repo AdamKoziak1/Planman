3bbe4c753f825504d3addcdb5a062a250eef6c1c
Youssef Zayed <yzaye1@ocdsb.ca>
2018-11-06 09:19:53 -0500

    added a home page

--- planman/projects/urls.py
+++ planman/projects/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projects_list, name='projects_list'),
-    path('<int:project_number>', views.tasks_list, name='tasks_list'),
+    path('', views.mainpage, name='mainpage'),
+    path('projects', views.projects_list, name='projects_list'),
+    path('projects/<int:project_number>', views.tasks_list, name='tasks_list'),
 ]
