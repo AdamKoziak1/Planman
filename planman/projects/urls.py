bd39b09a6f1d62f10a9a03934a637b5d74014114
Youssef Zayed <yzaye1@ocdsb.ca>
2018-11-06 18:56:08 -0500

    added home page

--- planman/projects/urls.py
+++ planman/projects/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('projects', views.projects_list, name='projects_list'),
+    path('projects/', views.projects_list, name='projects_list'),
     path('projects/<int:project_number>', views.tasks_list, name='tasks_list'),
 ]
