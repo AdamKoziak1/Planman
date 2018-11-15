094acd799a116fe6af82f4319bcf58f6d53ea5a7
Youssef Zayed <yzaye1@ocdsb.ca>
2018-11-15 11:19:21 -0500

    home page

--- planman/projects/urls.py
+++ planman/projects/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
+    path('home/', views.homepage, name='homepage'),
     path('projects/', views.projects_list, name='projects_list'),
     path('projects/<int:project_number>', views.tasks_list, name='tasks_list'),
     path('projects/<int:project_number>/task_create', views.task_create, name='task_create'),
-     path('projects/<int:project_number>/<int:task_number>/task_edit', views.task_edit, name='task_edit'),
+    path('projects/<int:project_number>/<int:task_number>/task_edit', views.task_edit, name='task_edit'),
 ]
