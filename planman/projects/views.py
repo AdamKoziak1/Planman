67bcc2e5c4ea1de0dcd079b15b064c28708cfd49
Youssef Zayed <yzaye1@ocdsb.ca>
2018-11-20 09:37:57 -0500

    delete button

--- planman/projects/views.py
+++ planman/projects/views.py
@@ -23,14 +23,14 @@ def homepage (request):
 
 @login_required
 def projects_list (request):
-    project_list = Project.objects.order_by('name')
+    project_list = Project.objects.order_by('date')
     context =  {"project_list" : project_list}
     return render(request, 'projects/projects.html',context)
 
 @login_required
 def tasks_list (request,project_number):
     task_list = Task.objects.filter(project= project_number)
-    context =  {"task_list" : task_list,"project" : Project.objects.filter(id = project_number)}
+    context =  {"task_list" : task_list,"project_id" : project_number, "project_name" :Project.objects.filter(id = project_number)}
     return render(request, 'projects/tasks.html',context)
 
 
