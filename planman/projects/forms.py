3f76907bcc9940cf73d3e63ba58c4079aaf81bfb
Adam Koziak <akozi1@ocdsb.ca>
2019-01-10 17:39:04 -0500

    fixed create/edit pages and made logos transparent +detailing

--- planman/projects/forms.py
+++ planman/projects/forms.py
@@ -7,11 +7,34 @@ from django import forms
 class Project_create(forms.ModelForm):
     class Meta:
         model = Project
+        fields = ('name', 'owner', 'description', 'cost', 'profit', 'start_date', 'end_date',)
+        labels = {
+            'name': ('Task Name'),
+            'owner': ('Project Owner'),
+            'description': ('Description'),
+            'cost': ('Cost'),
+            'profit': ('Profit'),
+            'start_date': ('Start Date'),
+            'end_date': ('End Date'),
+        }
         exclude = ['owner']
 
 class Task_form(forms.ModelForm):
     class Meta:
         model = Task
+        fields = ('name', 'description', 'cost', 'profit', 'start_date', 'end_date', 'hours_to_finish', 'hours_finished', 'project', 'parent_task')
+        labels = {
+            'name': ('Task Name'),
+            'description': ('Description'),
+            'cost': ('Cost'),
+            'profit': ('Profit'),
+            'start_date': ('Start Date'),
+            'end_date': ('End Date'),
+            'hours_to_finish': ('Hours Remaining'),
+            'hours_finished': ('Hours Completed'),
+            'project': ('Parent Project'),
+            'parent_task': ('Parent Task'),
+        }
         exclude = ['project']
 class Project_user(forms.ModelForm):
     class Meta:
